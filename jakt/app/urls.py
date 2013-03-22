f624d66908bc8ea9dc55eaf4d55d76ea8708967e
Joshua Kehn <josh.kehn@gmail.com>
2013-03-22 15:37:50 -0400

    Added error handling routes

--- jakt/app/urls.py
+++ jakt/app/urls.py
@@ -1,10 +1,16 @@
 # coding=utf-8
 from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import handler500, handler404, handler403
 from django.contrib import admin
 admin.autodiscover()
 
+# Setup error handlers
+handler500 = "frontend.views.internal_error"
+handler404 = "frontend.views.missing_error"
+handler403 = "frontend.views.forbidden_error"
+
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$',      include('frontend.urls')),
+    # Routes
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',      include('frontend.urls')),
 )
