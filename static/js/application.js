cc19596b0c13d2362ed45ac02bc35cc88ae13ce0
Anthony Tumbiolo <anthony.tumbiolo@gmail.com>
2013-04-26 12:03:16 -0400

    removed unnecessary stuff

--- static/js/application.js
+++
@@ -1,6 +0,0 @@
-ineedtodo = angular.module("ineedtodo", ["ngResource"]);
-
-ineedtodo.config(function($interpolateProvider) {
-  $interpolateProvider.startSymbol("{[{");
-  $interpolateProvider.endSymbol("}]}");
-});
--- static/js/directives/todoBlur.js
+++
@@ -1,7 +0,0 @@
-ineedtodo.directive("todoBlur", function () {
-    return function ($scope, element, attributes) {
-        element.bind("blur", function () {
-            $scope.$apply(attributes.todoBlur)
-        })
-    }
-})
